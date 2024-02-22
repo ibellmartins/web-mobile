4be00c5b78d5332635a92dc25f20d8c1779b8c2f
Isabella Sofia Martins <106943011+ibellmartins@users.noreply.github.com>
2024-02-22 10:59:14 -0300

    Add files via upload

---
+++ aula 4/script.js
@@ -0,0 +1,45 @@
+//ex.1 - crie uma função que receba 3 numeros e retorn a média aritmética deles
+function media(n1, n2, n3){
+    return (n1+n2+n3)/3
+}
+var result = media(5, 6, 10)
+console.log("média aritmética: "+result);
+
+
+//ex.2 - crie uma função que receba 6 números (3 notas e 3 pesos). Retorne a média ponderada.
+function mediaPonderada(n1, n2, n3, p1, p2, p3){
+    return (n1*p1+n2*p2+n3*p3)/p1+p2+p3
+}
+var restultMedia = mediaPonderada(10, 5, 10, 1, 2, 3)
+console.log("média ponderada: "+restultMedia)
+
+
+//ex.3 - crie uma função que receba uma string com valores 0s e 1s, representando um binário e devola seu valor inteiro positivo. 
+// function binario(bin){
+//     let res = 0 
+//     let num = bin.split("")
+
+// }
+
+// var resultBinario = binario(101)
+// console.log("resultado: "+)
+
+
+//ex.4 - crie um algoritmo que receba uma frase e a mostre na lingua do P. 
+// let texto = "Eu gosto do Mackenzie" ;
+// const array = texto.split(" ");
+// for (var i = array; i < texto.length; i++){
+//     const texto = texto[i] + "P" + texto[i] 
+// }
+// console.log(texto)
+// return arrayP 
+
+
+//ex.5 - crie um algoritmo que receba 3 strings e substitua na primeira, todas as ocorrências da segunda pela terceira. 
+function frase(p1, p2, p3){
+    
+    
+}
+
+
+
